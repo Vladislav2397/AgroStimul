17224604e9c80bf9484da1180c19dd44fb266393
anonymous <valdiszz53@gmail.com>
2020-07-30 20:13:14 +0300

    Some adaptation on tablet and phone

--- libraries/js/carousel.js
+++ libraries/js/carousel.js
@@ -5,11 +5,24 @@ var strStart = "translateX(-";
 var strEnd = "px)";
 
 var items = document.getElementsByClassName("carousel-item");
+var carousel = document.getElementsByClassName("carousel");
 
 var carouselSlide = function(move) {
+
+    var lenCarousel = carousel[0].offsetWidth;
+    console.log(lenCarousel);
+    var numOfStep = 0;
+    if (lenCarousel == 270) {
+        numOfStep = 4;
+    } else if (lenCarousel == 270 * 2) {
+        numOfStep = 3;
+    } else {
+        numOfStep = 2;
+    }
+
     if (move === "left" && pos >= step)
         pos -= step;
-    else if (move == "right" && pos <= step * 2)
+    else if (move == "right" && pos <= step * numOfStep)
         pos += step;
     for (var i = 0; i < items.length; i++) {
         items[i].style.transform = strStart + pos + strEnd;
